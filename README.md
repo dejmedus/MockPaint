d1aa82c4426441600b219ac4a562ef7a4e2ec663
dejmedus <email96123@gmail.com>
2022-10-27 23:32:02 -0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,27 @@
+
+## MockPaint 🎨
+
+### Remake a Paint App
+
+MockPaint is a pared-down clone of MS Paint created with [P5.js](https://p5js.org) for [Global Hack Week's](https://ghw.mlh.io/oct) Remake a Paint App challenge.
+
+Currently you can:
+
+- Draw with a pen or brush
+- Set a brush width (max 1000)
+- Erase your work
+- Choose from 8 colors
+- Change the background color with bucket
+- Clear the canvas
+
+Still to be implemented:
+
+- Shapes
+- Proper fill/bucket functionality
+- Choose your own color palette
+- Switch between 2 primary colors
+- UI Themes
+
+#### Setup
+
+To setup and view this project in the browser I used the p5.vscode and Live Server VSCode extensions. For alternatives, see the P5.js [Get Started](https://p5js.org/get-started/) guide.
---
+++ index.js
@@ -0,0 +1,98 @@
+
+const palette = {
+  'black': '#000',
+  'white': '#fff',
+  'red': '#ff0000',
+  'orange': '#ffa500',
+  'yellow': '#ffff00',
+  'green': '#008000',
+  'light-green': '#affc41',
+  'blue': '#0000ff',
+  'light-blue': '#90e0ef',
+  'indigo': '#4b0082',
+  'violet': '#ee82ee',
+  'brown': '#774936',
+}
+
+let curColor = palette.black;
+let brushWidth = 12;
+let curCursor = 'pen';
+let bgColor = '#fff';
+
+function setup() {
+  createCanvas((windowWidth - 50), (windowHeight - 80));
+  background(bgColor);
+}
+
+function draw() {
+
+  switch (curCursor) {
+    case 'pen':
+      cursor('images/pen.png');
+      break;
+    case 'brush':
+      cursor('images/brush.png');
+      break;
+    case 'bucket':
+      cursor('images/bucket.png');
+      break;
+    case 'eraser':
+      cursor('images/eraser.png');
+      break;
+    default:
+      cursor(CROSS);
+      break;
+  }
+
+  curCursor == 'pen'
+    ? strokeWeight(1)
+    : strokeWeight(brushWidth)
+
+  curCursor == 'eraser'
+    ? (stroke(color(bgColor)), strokeWeight(brushWidth))
+    : stroke(color(curColor))
+
+  // Only draw if the mouse is pressed inside the canvas
+  // Currently, bucket only changes the background color
+  if ((mouseX >= 0) && (mouseX <= width) && (mouseY <= height) && (mouseY >= 0)) {
+    if (mouseIsPressed === true) {
+      curCursor == 'bucket'
+        ? (bgColor = curColor, background(bgColor))
+        : line(mouseX, mouseY, pmouseX, pmouseY)
+    }
+  }
+}
+
+function brushColor(color) {
+  curColor = palette[color.id] || pallette.black;
+}
+
+// Cap brush size at 980
+function brushSize(size) {
+  brushWidth = (size.value > 989) ? '980' : size.value
+}
+
+function selectBrush() {
+  curCursor = 'brush';
+}
+function selectPen() {
+  curCursor = 'pen';
+}
+function selectEraser() {
+  curCursor = 'eraser';
+}
+function selectBucket() {
+  curCursor = 'bucket';
+}
+
+
+function clearScreen() {
+  clear()
+  bgColor = '#fff';
+  background(bgColor);
+}
+
+function windowResized() {
+  resizeCanvas((windowWidth - 50), (windowHeight - 80));
+  background(bgColor);
+}
